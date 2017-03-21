c05a0c27450bf5ed1cc0e47cafe669e969bdfa9c
Scott Schepers <schepers.scott@hotmail.com>
2017-03-21 10:27:55 +0100

    step 2: Completed

---
+++ Centipede/Centipede/Player.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Centipede
+{
+    class Player : SpriteGameObject
+    {
+
+        public Player()
+            :base("spr_player")
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            position = Vector2.Clamp(position, Vector2.Zero, new Vector2(Centipede.Screen.X - Width, Centipede.Screen.Y - Height));
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            velocity = Vector2.Zero;
+            if (inputHelper.IsKeyDown(Keys.Up)) velocity.Y = -200;
+            if (inputHelper.IsKeyDown(Keys.Down)) velocity.Y = 200;
+            if (inputHelper.IsKeyDown(Keys.Left)) velocity.X = -200;
+            if (inputHelper.IsKeyDown(Keys.Right)) velocity.X = 200;
+        }
+    }
+}
--- Centipede/Centipede/PlayingState.cs
+++ Centipede/Centipede/PlayingState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,18 @@ namespace Centipede
     class PlayingState : GameObjectList
     {
         SpriteGameObject background;
+        GameObject player;
         public PlayingState()
         {
             background = new SpriteGameObject("spr_background");
+            player = new Player();
+            player.Position = new Vector2(235, 500);
+
 
             this.Add(background);
+            this.Add(player);
         }
+
+
     }
 }
